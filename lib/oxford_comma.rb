7be293c6c0c0760fb4ac69a4efafa72339be9c5d
bright-reducer-1915 <cristi.nord@gmail.com>
2018-10-18 16:42:46 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,7 @@ def oxford_comma(array)
   if array.length == 1
     return array[0]
   elsif array.length == 2
-    return array.join(" and ")
+    arr_and = array.join(" and ")
+    return arr_and
   end
 end
