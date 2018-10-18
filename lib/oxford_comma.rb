865e724fb87fa9cf35d90ecf3d3e0f116467e2d6
bright-reducer-1915 <cristi.nord@gmail.com>
2018-10-18 17:01:11 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,14 @@
 
-
 def oxford_comma(array)
-  if array.length == 1
-    return array[0]
-  elsif array.length == 2
-    arr_and = array.join(" and ")
-    return arr_and
-  elsif array.length === 3
-    string = " and " + array[2]
-    
-  end
+  if array.length == 1 
+  array.join('')
+  
+  elsif array.length == 2 
+    array.join(' and ')
+    elsif array.length >= 3 
+      array[-1] = "and #{array[-1]}"
+      array.join(', ')
+    else 
+      nil 
+    end 
 end
-
